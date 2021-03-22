2fa805e20d3262d89a95f3703278b94e4f39fabb
Kevin Bache <kevin.bache@gmail.com>
2021-03-22 00:27:13 -0600

    transfer

--- verse_monster/train.py
+++ verse_monster/train.py
@@ -131,21 +131,21 @@ if __name__ == '__main__':
             if num_valid is not None:
                 ds_valid = ds_valid[:num_valid]
 
-            # keys_to_remove = ('decoder_input_ids', 'decoder_attention_mask')
-            #
-            # for dp in ds_train:
-            #     for k in keys_to_remove:
-            #         if k in dp:
-            #             print(f'removing {k}')
-            #             del dp[k]
-            # print(f'dp.keys(): {dp.keys()}')
-            #
-            # for dp in ds_valid:
-            #     for k in keys_to_remove:
-            #         if k in dp:
-            #             print(f'removing {k}')
-            #             del dp[k]
-            # print(f'dp.keys(): {dp.keys()}')
+            keys_to_remove = ('decoder_input_ids', 'decoder_attention_mask')
+
+            for dp in ds_train:
+                for k in keys_to_remove:
+                    if k in dp:
+                        print(f'removing {k}')
+                        del dp[k]
+            print(f'dp.keys(): {dp.keys()}')
+
+            for dp in ds_valid:
+                for k in keys_to_remove:
+                    if k in dp:
+                        print(f'removing {k}')
+                        del dp[k]
+            print(f'dp.keys(): {dp.keys()}')
 
             # ds_test = utils.load_cloudpickle(constants.TEST_DATASET)
 
